e9201fd7f4c2262a5d3ae6d686288fc2c97b0ec4
Alejandro Sanchez <alejandroszg@gmail.com>
2022-03-21 22:26:22 -0300

    Iniciando con Arrays

---
+++ Curso JS Moderno/08-Arrays/js/01-app.js
@@ -0,0 +1,12 @@
+const numeros = [10];
+
+const meses = new Array('Enero', 'Febrero', 'Marzo');
+
+console.log(numeros);
+console.log(meses)
+
+// Los arreglos/arrays contienen todo tipo de dato
+
+const deTodo = ['Hola', 27, true, null, {nombre: 'Alejandro', edad: 27}, [1, 2,3]];
+
+console.log(deTodo)
