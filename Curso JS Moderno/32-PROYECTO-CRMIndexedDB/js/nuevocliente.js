4210b2c65f6d4f999b3a02ee159fe96118e99bdf
Alejandro Sanchez <alejandroszg@gmail.com>
2022-05-18 19:29:23 -0400

    proyecto11-CRMIndexedDB

---
+++ Curso JS Moderno/32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -0,0 +1,56 @@
+(function(){
+    const formulario = document.querySelector('#formulario');
+
+    document.addEventListener('DOMContentLoaded', () => {
+        conectarDB();
+
+        formulario.addEventListener('submit', validarCliente);
+    });
+
+    function validarCliente(e){
+        e.preventDefault();
+
+        // Leer inputs
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+        if(nombre === '' || email === '' || telefono === '' || empresa === ''){
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
+            return;
+        };
+
+        // Crear objeto con la información
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa,
+        };
+        cliente.id = Date.now();
+        
+        crearNuevoCliente(cliente);
+    }
+
+    function crearNuevoCliente(cliente){
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+        
+        objectStore.add(cliente);
+        
+        transaction.onerror = () => {
+            console.log('Hubo un error');
+            imprimirAlerta('Hubo un error', 'error');
+        };
+
+        transaction.oncomplete = () => {
+            console.log('Cliente agregado');
+
+            imprimirAlerta('Cliente se agregó correctamente');
+            setTimeout(()=>{
+                window.location.href = 'index.html';
+            }, 3000)
+        };
+    }
+})();
