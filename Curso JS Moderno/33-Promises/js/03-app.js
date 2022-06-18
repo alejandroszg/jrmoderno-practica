2df127696c799253bcaebe9ed342c86cb8ede2fe
Alejandro Sanchez <alejandroszg@gmail.com>
2022-06-18 19:28:41 -0400

    callback, promises

---
+++ Curso JS Moderno/33-Promises/js/03-app.js
@@ -0,0 +1,34 @@
+// const aplicarDescuento = new Promise((resolve, revejct) => {
+//     const descuento = true;
+
+//     if(descuento){
+//         resolve('Descuento aplicado');
+//     } else {
+//         revejct('No se pudo aplicar');
+//     }
+// })
+
+// aplicarDescuento
+//     .then(resultado => console.log(resultado))
+//     .catch(error => console.log(error))
+
+
+// // 3 Valores disponibles
+// // fullfilled - Se cumplió el promise
+// // rejected - No se cumplió el proimse
+// // pending - No se cumplió el promise y no fue rechazado
+
+// function descuento(mensaje){
+//     console.log(mensaje)
+// }
+
+
+let promesa = new Promise ((resolve, reject)=> {
+    setTimeout(() => {
+        resolve('Timeout');
+    }, 1000);
+});
+
+promesa.then((data) =>{
+    console.log(data)
+})
